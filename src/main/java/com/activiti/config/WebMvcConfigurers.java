96409273dbd5f8b64013bcdf1090fd3363234c89
joelan <joelan0927@aliyun.com>
2019-07-16 00:44:03 +0800

    初步提交-SpringBoot2.1.6 + activiti-modeler6.0设计器集成

---
+++ src/main/java/com/activiti/config/WebMvcConfigurers.java
@@ -0,0 +1,28 @@
+package com.activiti.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 加载静态资源类
+ */
+@Configuration
+public class WebMvcConfigurers implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+
+}
